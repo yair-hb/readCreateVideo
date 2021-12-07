45e0f9551a50651571115d647c8233bb66e33dda
yair <aythb@hotmail.com>
2021-12-07 16:54:43 -0600

    se corrige el codigo del mostrar un video (webcam)

--- readCreateVideo.py
+++ readCreateVideo.py
@@ -1,11 +1,12 @@
+#en este codigo se crea una ventana en la que se muestra una fuente de video (webcam)
 import cv2
 
-captura = cv2.VideoCapture('video.mp4')
+captura = cv2.VideoCapture(0)
 while (captura.isOpened()):
     ret, imagen =captura.read()
     if ret == True:
         cv2.imshow('captura', imagen)
-        if cv2.waitKey(30) == ord('s'):
+        if cv2.waitKey(30) & 0xFF == ord('s'):
             break
     else: break
 captura.release()
